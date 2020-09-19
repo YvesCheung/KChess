59b3a4853f653455a63c2fc8abde3c7e9028adc2
YvesCheung <975135274@qq.com>
2020-09-19 17:52:02 +0800

    Use npm workspace link instead of remote download

--- ReactNative/__tests__/App-test.js
+++ ReactNative/__tests__/App-test.js
@@ -4,17 +4,23 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../app/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+    renderer.create(<App/>);
 });
 
 it('test kotlin2js', function () {
-  const algorithm = require("KChess/KChess-algorithm-chinesechess")
-  const game = new algorithm.com.github.kchess.algorithm.ChineseChess()
-  console.log(game.gameBroad)
+    const algorithm = require("kchess-algorithm-chinesechess")
+    const game = new algorithm.ChineseChess()
+    game.gameBroad.forEach((newLine, chessman) => {
+        if (chessman) {
+            console.log(chessman.desc)
+        } else {
+            console.log('+')
+        }
+    })
 });
