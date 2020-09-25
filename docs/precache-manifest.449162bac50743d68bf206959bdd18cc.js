a46062164600faeddde33113a4dab5144fd3f2de
YvesCheung <975135274@qq.com>
2020-09-25 18:20:34 +0800

    Build the static website to ../docs

---
+++ docs/precache-manifest.449162bac50743d68bf206959bdd18cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cbed87610fd80da4e7ee24b941bf6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a38bdf52f1d9e789f50",
+    "url": "./static/css/main.fb78e445.chunk.css"
+  },
+  {
+    "revision": "daec0d643adb763620f7",
+    "url": "./static/js/2.266c4a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.266c4a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a38bdf52f1d9e789f50",
+    "url": "./static/js/main.c83b073c.chunk.js"
+  },
+  {
+    "revision": "ebf2e20514efc33ab165",
+    "url": "./static/js/runtime-main.271fd613.js"
+  },
+  {
+    "revision": "f2845c5c780202ad13eb85014cc484d5",
+    "url": "./static/media/bg.f2845c5c.png"
+  },
+  {
+    "revision": "42111d0dc07faeb6f244ca411736780e",
+    "url": "./static/media/body.42111d0d.jpg"
+  }
+]);
